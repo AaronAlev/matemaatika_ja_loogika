02d02a92c70375db674e6b332bae2db889e448c4
Aaron <aaronalev@gmail.com>
2022-11-09 12:42:39 +0200

    Ülesanne: Funktsioon

---
+++ functions.py
@@ -0,0 +1,31 @@
+"""Functions"""
+
+
+# func()
+def func():
+    """Print "I´m inside the function."""
+    print("I´m inside the function")
+
+
+# my_name_is(name)
+def my_name_is(name):
+    """Print "My name is" and inputted name."""
+    print(f"My name is {name}")
+
+
+# sum_six(num)`
+def sum_six(num: int):
+    """Return the sum of 6 and inputted number."""
+    return 6 + num
+
+
+# sum_numbers()
+def sum_numbers(a: int, b: int):
+    """Return the sum of guven numbers."""
+    return a + b
+
+
+# usd_to_eur()
+def usd_to_eur(usd: int):
+    """Calculate usd to eur on 0.8 base."""
+    return usd * 0.8
